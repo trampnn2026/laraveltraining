20f1309a31fc62137b59031fb6a7f193f1e7c81b
trampnn95 <trampnn2605@gmail.com>
2024-11-05 16:20:31 +0700

    CRUD

--- database/migrations/2024_11_01_071058_create_profiles_table.php
+++ database/migrations/2024_11_01_071058_create_profiles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(User::class)->nullable()->constrained()->cascadeOnDelete();
             $table->string('name')->nullable();
             $table->string('fullname')->nullable();
             $table->string('phone', length: 10)->nullable();
